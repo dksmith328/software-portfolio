252d4c472100d316c0046ed9000ea59e3e897fd8
dksmith328 <>
2020-11-14 14:23:12 -0700

    added Procfile and requirements.txt

--- site.py
+++ site.py
@@ -1,8 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
-from wtforms import Form
-from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
